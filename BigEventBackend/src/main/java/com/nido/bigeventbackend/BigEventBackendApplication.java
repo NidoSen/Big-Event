effa09b5f85ff01548c41019368bc3b071a265d9
NidoSen <2820147082@qq.com>
2023-11-20 15:12:49 +0800

    完成环境搭建

---
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/BigEventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nido.bigeventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigEventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigEventBackendApplication.class, args);
+    }
+
+}
