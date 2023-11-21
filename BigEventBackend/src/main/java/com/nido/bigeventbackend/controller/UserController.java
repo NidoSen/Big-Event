e7db3bf878413c3a0d1aff49a3f1be2a8a39867e
NidoSen <2820147082@qq.com>
2023-11-21 13:25:37 +0800

    完成后端的更新用户头像的功能

--- BigEventBackend/src/main/java/com/nido/bigeventbackend/controller/UserController.java
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/controller/UserController.java
@@ -7,6 +7,7 @@ import com.nido.bigeventbackend.utils.JwtUtil;
 import com.nido.bigeventbackend.utils.Md5Util;
 import com.nido.bigeventbackend.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -77,4 +78,9 @@ public class UserController {
         return Result.success();
     }
 
+    @PatchMapping("/updateAvatar")
+    public Result updateAvatar(@RequestParam @URL String avatarUrl) {
+        userService.updateAvatar(avatarUrl);
+        return Result.success();
+    }
 }
--- BigEventBackend/src/main/java/com/nido/bigeventbackend/service/UserService.java
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/service/UserService.java
@@ -4,10 +4,15 @@ import com.nido.bigeventbackend.pojo.User;
 
 public interface UserService {
 
+    // 根据用户名查询用户
     public User findByUsername(String username);
 
+    // 注册
     public void register(String username, String password);
 
     // 更新
     public void update(User user);
+
+    // 更新头像
+    public void updateAvatar(String avatarUrl);
 }
