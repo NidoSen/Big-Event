a0ec632572fafd12e15ccb3f5498385040eae3f2
NidoSen <2820147082@qq.com>
2023-11-21 12:15:54 +0800

    完成后端的获取用户详细信息的功能

--- BigEventBackend/src/main/java/com/nido/bigeventbackend/controller/UserController.java
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/controller/UserController.java
@@ -5,12 +5,11 @@ import com.nido.bigeventbackend.pojo.User;
 import com.nido.bigeventbackend.service.UserService;
 import com.nido.bigeventbackend.utils.JwtUtil;
 import com.nido.bigeventbackend.utils.Md5Util;
+import com.nido.bigeventbackend.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -58,6 +57,18 @@ public class UserController {
         else {
             return Result.error("密码错误");
         }
+    }
+
+    @GetMapping ("/userInfo")
+    public Result<User> userInfo(/*@RequestHeader(name = "Authorization") String token*/) {
+        // 根据用户名查询用户
+        /*Map<String, Object> map = JwtUtil.parseToken(token);
+        String username = (String)map.get("username");*/
+        Map<String, Object> map = ThreadLocalUtil.get();
+        String username = (String)map.get("username");
+
+        User user = userService.findByUsername(username);
+        return Result.success(user);
     }
 
 }
--- BigEventBackend/src/main/java/com/nido/bigeventbackend/interceptors/LoginInterceptor.java
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/interceptors/LoginInterceptor.java
@@ -2,6 +2,7 @@ package com.nido.bigeventbackend.interceptors;
 
 import com.nido.bigeventbackend.pojo.Result;
 import com.nido.bigeventbackend.utils.JwtUtil;
+import com.nido.bigeventbackend.utils.ThreadLocalUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,8 @@ public class LoginInterceptor implements HandlerInterceptor {
         // 验证token
         try {
             Map<String, Object> claims =  JwtUtil.parseToken(token);
+            // 把业务数据存储到ThreadLocal中
+            ThreadLocalUtil.set(claims);
             // 放行
             return true;
         } catch (Exception e) {
@@ -27,5 +30,11 @@ public class LoginInterceptor implements HandlerInterceptor {
             // 不放行
             return false;
         }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 清空ThreadLocal中的数据
+        ThreadLocalUtil.remove();
     }
 }
