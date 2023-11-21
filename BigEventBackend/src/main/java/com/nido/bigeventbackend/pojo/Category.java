ece313de0dde1c75461c6cb14764b249d5aa3b97
NidoSen <2820147082@qq.com>
2023-11-21 19:08:18 +0800

    完成后端的文章分类列表功能

--- BigEventBackend/src/main/java/com/nido/bigeventbackend/pojo/Category.java
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/pojo/Category.java
@@ -1,5 +1,6 @@
 package com.nido.bigeventbackend.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,8 @@ public class Category {
     @NotEmpty
     private String categoryAlias;//分类别名
     private Integer createUser;//创建人ID
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
     private LocalDateTime createTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
 }
--- BigEventBackend/src/main/java/com/nido/bigeventbackend/service/CategoryService.java
+++ BigEventBackend/src/main/java/com/nido/bigeventbackend/service/CategoryService.java
@@ -2,8 +2,13 @@ package com.nido.bigeventbackend.service;
 
 import com.nido.bigeventbackend.pojo.Category;
 
+import java.util.List;
+
 public interface CategoryService {
 
     // 新增分类
     public void add(Category category);
+
+    // 列表查询
+    public List<Category> list();
 }
