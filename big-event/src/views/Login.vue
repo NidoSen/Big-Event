188877b49c8716e8f8dbee827e9f3d056e51eaef
NidoSen <2820147082@qq.com>
2023-12-02 23:53:25 +0800

    完成前端的登录功能

--- big-event/src/views/Login.vue
+++ big-event/src/views/Login.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { User, Lock } from "@element-plus/icons-vue";
 import { ref } from "vue";
+import { ElMessage } from "element-plus";
 //控制注册与登录表单的显示， 默认显示注册
 const isRegister = ref(false);
 //定义数据模型
@@ -35,17 +36,44 @@ const rules = {
 };
 
 //调用后台接口完成注册
-import { userRegisterService } from "@/api/user.js";
+import { userRegisterService, userLoginService } from "@/api/user.js";
 const register = async () => {
   //registerData是一个响应式对象，如果要获取值，需要.value
   let result = await userRegisterService(registerData.value);
-  if (result.code === 0) {
+  /* if (result.code === 0) {
     //成功了
     alert(result.msg ? result.msg : "注册成功");
   } else {
     //失败了
     alert("注册失败");
-  }
+  } */
+  //alert(result.msg ? result.msg : "注册成功");
+  ElMessage.success(result.msg ? result.msg : "注册成功");
+};
+
+//绑定数据，复用注册表单的数据模型
+//表单数据校验
+//登录函数
+const login = async () => {
+  let result = await userLoginService(registerData.value);
+  /* if (result.code === 0) {
+    //成功了
+    alert(result.msg ? result.msg : "登录成功");
+  } else {
+    //失败了
+    alert("登录失败");
+  } */
+  //alert(result.msg ? result.msg : "登录成功");
+  ElMessage.success(result.msg ? result.msg : "登录成功");
+};
+
+//定义函数，清空数据模型的数据
+const clearRegisterData = () => {
+  registerData.value = {
+    username: "",
+    password: "",
+    rePassword: "",
+  };
 };
 </script>
 
@@ -100,25 +128,44 @@ const register = async () => {
           </el-button>
         </el-form-item>
         <el-form-item class="flex">
-          <el-link type="info" :underline="false" @click="isRegister = false">
+          <el-link
+            type="info"
+            :underline="false"
+            @click="
+              isRegister = false;
+              clearRegisterData;
+            "
+          >
             ← 返回
           </el-link>
         </el-form-item>
       </el-form>
       <!-- 登录表单 -->
-      <el-form ref="form" size="large" autocomplete="off" v-else>
+      <el-form
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-else
+        :model="registerData"
+        :rules="rules"
+      >
         <el-form-item>
           <h1>登录</h1>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        <el-form-item prop="username">
+          <el-input
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+            v-model="registerData.username"
+          ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             name="password"
             :prefix-icon="Lock"
             type="password"
             placeholder="请输入密码"
+            v-model="registerData.password"
           ></el-input>
         </el-form-item>
         <el-form-item class="flex">
@@ -129,12 +176,23 @@ const register = async () => {
         </el-form-item>
         <!-- 登录按钮 -->
         <el-form-item>
-          <el-button class="button" type="primary" auto-insert-space
+          <el-button
+            class="button"
+            type="primary"
+            auto-insert-space
+            @click="login"
             >登录</el-button
           >
         </el-form-item>
         <el-form-item class="flex">
-          <el-link type="info" :underline="false" @click="isRegister = true">
+          <el-link
+            type="info"
+            :underline="false"
+            @click="
+              isRegister = true;
+              clearRegisterData;
+            "
+          >
             注册 →
           </el-link>
         </el-form-item>
