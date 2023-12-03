ba615ee095b48bc8246257cac2f291bdab9f4681
NidoSen <2820147082@qq.com>
2023-12-03 13:52:56 +0800

    完成主页面和路由

--- big-event/src/main.js
+++ big-event/src/main.js
@@ -1,11 +1,15 @@
 import './assets/main.scss'
 
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import router from '@/router/index.js'
 
 import App from './App.vue'
 
 const app = createApp(App);
+app.use(router);
 app.use(ElementPlus);
 app.mount('#app')
