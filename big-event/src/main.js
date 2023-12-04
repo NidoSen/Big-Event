a399f474e5d131a75f63c2f945d9a461f3179107
NidoSen <2820147082@qq.com>
2023-12-04 15:03:51 +0800

    完成前端的文章列表查询功能

--- big-event/src/main.js
+++ big-event/src/main.js
@@ -14,12 +14,15 @@ import {
 } from 'pinia-persistedstate-plugin'
 
 import App from './App.vue'
+import locale from 'element-plus/dist/locale/zh-cn.js'
 
 const app = createApp(App);
 const pinia = createPinia();
 const persist = createPersistedState();
 app.use(router);
-app.use(ElementPlus);
+app.use(ElementPlus, {
+    locale
+})
 pinia.use(persist);
 app.use(pinia);
 app.mount('#app')
